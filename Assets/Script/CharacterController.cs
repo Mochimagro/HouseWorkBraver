b63c048ad9b3b18b6cb14e6dac7e015aceffcbb8
Mochimagro <tasklog2@outlook.jp>
2018-09-19 03:16:01 +0900

    初コミット

---
+++ Assets/Script/CharacterController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+	public float speed = 5f;
+	public float smoothRotation = 10f;
+	public float animationSpeed = 1.5f;
+
+	private Rigidbody playerRigidbody;
+	private Animator animator;
+	
+
+	void Start () {
+		playerRigidbody = GetComponent<Rigidbody>();
+		animator = GetComponent<Animator>();
+	}
+	
+    
+
+	void FixedUpdate () {
+		var dh = Input.GetAxis("Horizontal");
+		var dv = Input.GetAxis("Vertical");
+
+		//var velosity = new Vector3(dh,0,dv) * speed;
+
+		var targetVel = new Vector3(dh,0,dv);
+
+		animator.SetFloat("Speed",targetVel.magnitude);
+		animator.speed = animationSpeed;
+
+		if(targetVel.magnitude > 0.1){
+			
+			Quaternion rotation = Quaternion.LookRotation(targetVel);
+			transform.rotation = Quaternion.Lerp(transform.rotation,rotation,Time.deltaTime * smoothRotation);
+			
+
+			transform.localPosition += targetVel * speed * Time.fixedDeltaTime;
+		}
+		
+
+	}
+}
---
+++ Assets/Script/SubCharacterController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SubCharacterController : MonoBehaviour {
+	public GameObject target;
+
+	private NavMeshAgent agent;
+	private Animator animator;
+
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+		animator = GetComponent<Animator>();
+		animator.speed = 1.5f;
+
+        // ターゲットの位置を目的地に設定する。
+        agent.destination = target.transform.position;
+    }
+
+	void Update () {
+
+		animator.SetFloat("Speed",agent.velocity.magnitude);
+
+		
+	}
+}
+
