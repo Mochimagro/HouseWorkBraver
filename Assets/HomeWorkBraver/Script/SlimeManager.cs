0e2fa1e06002f0ce6b7cf41f229768c7a947d726
Mochimagro <tasklog2@outlook.jp>
2018-09-21 02:12:42 +0900

    HWB-03 撃破エフェクトの追加
    エフェクトはResousesファイルないで呼出を行っている

--- Assets/HomeWorkBraver/Script/SlimeManager.cs
+++ Assets/HomeWorkBraver/Script/SlimeManager.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class SlimeManager : MonoBehaviour {
 
+	private GameObject effectKnockdoun;
+
+	private void Start() {
+		effectKnockdoun = (GameObject) Resources.Load("Effect/EffectKnockdown");
+	}
+
 	public void KnockDown(){
 		Destroy(gameObject);
+		var eff = Instantiate(effectKnockdoun,transform.position,Quaternion.identity);
 	}
 
 }
