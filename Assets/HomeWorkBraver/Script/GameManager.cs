d5bbe6424a6df91dea4034ccd410351a48d358c7
Mochimagro <tasklog2@outlook.jp>
2018-09-24 02:25:34 +0900

    HWB-10 レベル追加

--- Assets/HomeWorkBraver/Script/GameManager.cs
+++ Assets/HomeWorkBraver/Script/GameManager.cs
@@ -7,21 +7,43 @@ using UnityEngine;
 public class GameManager : MonoBehaviour {
 
 	private int braverExp;
-	private int nextExp = 500;
+	private int nextExp = 10;
+	private int braverLevel = 1;
 	public TextMeshProUGUI TextExp;
+	public TextMeshProUGUI TextLevel;
 
 	private void Start() {
 		braverExp = 0;
+		nextExp = 10;
+		braverLevel = 1;
 		SetExpText();
+		SetLevelText();
 	}
 
 	public void AddEXP(int value){
 		braverExp += value;
+		if(braverExp >= nextExp){
+			LevelUp();
+		}
 		SetExpText();
 	}
 
 	private void SetExpText(){
 		TextExp.text = braverExp + " / " + nextExp;
+	}
+
+	private void SetLevelText(){
+		TextLevel.text = "Lv." + braverLevel;
+	}
+
+	private void LevelUp(){
+		braverExp -= nextExp;
+		braverLevel++;
+		nextExp += 50;
+		if(braverExp >= nextExp){
+			LevelUp();
+		}
+		SetLevelText();
 	}
 
 }
