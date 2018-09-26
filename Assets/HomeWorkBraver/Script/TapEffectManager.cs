55b2a87ec080f3178bd6e33aaa42e4541053157c
Mochimagro <tasklog2@outlook.jp>
2018-09-26 17:32:33 +0900

    HWB-14 タップエフェクトの追加

---
+++ Assets/HomeWorkBraver/Script/TapEffectManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapEffectManager : MonoBehaviour {
+
+	public ParticleSystem tapEffect;
+	public Camera effectCamera;
+
+	private void Start() {
+		
+	}
+
+	private void Update() {
+		if(Input.GetMouseButtonDown(0)){
+			var pos = effectCamera.ScreenToWorldPoint(Input.mousePosition + effectCamera.transform.forward * 10);
+			tapEffect.transform.position = pos;
+			tapEffect.Emit(Random.Range(4,7));
+		}
+	}
+
+}
