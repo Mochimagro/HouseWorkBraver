abdf261419bd58858a75bc5db2d7e05522c9fea4
Mochimagro <tasklog2@outlook.jp>
2018-09-30 05:10:19 +0900

    HWB-20 HPゲージの追加、Intervalゲージの追加

--- Assets/HomeWorkBraver/Script/Quest/BraverQuestManager.cs
+++ Assets/HomeWorkBraver/Script/Quest/BraverQuestManager.cs
@@ -1,14 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class BraverQuestManager : MonoBehaviour {
 
     public int hp = 50;
+    private int maxHp;
     public int atk = 3;
     public float speed = 1.5f;
     public float intervalTime = 5.0f;
     public GameObject Weapon;
+    public Slider SliderHpGauge;
+    public TextMeshProUGUI TextHp;
+    
+    public GameObject SliderIntervalGuage;
+    private Slider intervalSlider;
 
 	private Animator animator;
     private SphereCollider attackArea;
@@ -23,13 +31,15 @@ public class BraverQuestManager : MonoBehaviour {
         animator.speed = speed;
         animator.SetFloat("Speed",1);
         pos = transform.position;
-        
+        maxHp = hp;
+        SliderHpGauge.maxValue = maxHp;
+        intervalSlider = SliderIntervalGuage.GetComponent<Slider>();
+        intervalSlider.maxValue = intervalTime;
+        SetHpUI();
     }
 
     private void Update() {
-        if(interval > 0){
-            interval -= Time.deltaTime;
-        }
+        SetIntervalSlider();
     }
 
     private void OnTriggerEnter(Collider other) {
@@ -44,12 +54,34 @@ public class BraverQuestManager : MonoBehaviour {
     public void Damage(int atk){
         hp -= atk;
         if(hp <= 0){
+            hp = 0;
             Death();
         }
+        SetHpUI();
     }
 
     private void Death(){
         animator.SetTrigger("Death");
+    }
+
+    public void SetHpUI(){
+        TextHp.text = hp + " / " + maxHp;
+        SliderHpGauge.value = hp;
+    }
+
+    private void SetIntervalSlider(){
+        
+        if(interval > 0){
+            if(!SliderIntervalGuage.activeSelf){
+                SliderIntervalGuage.SetActive(true);
+            }
+            interval -= Time.deltaTime;
+            intervalSlider.value = interval;
+            if(interval <= 0){
+                SliderIntervalGuage.SetActive(false);
+            }
+        }
+        
     }
 
     //アニメーション用
